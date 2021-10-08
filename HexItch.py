ab947bfe9dad13cfa779d8ed9ff02a9216ee3227
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2021-10-08 19:17:51 -0300

    load file from cmdline and draw addresses and real filesize info

--- HexItch.py
+++ HexItch.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+#
+# HexItch - hex-editor
+#
+# (c) 2021 Felipe Correa da Silva Sanches <juca@members.fsf.org>
+# Released under the terms of the GNU General Public License version 3 or later
+
 import curses
 import os
 import sys
@@ -7,11 +14,11 @@ VERSION = "v0.0.1"
 RELEASE_YEAR = "2021"
 
 context = {
-    "filesize": "14532k", #FIXME
-    "filesize_hex": "000830F6", #FIXME
-    "filename": "~/nonfree/MSDOS/GAMES/PFANT/TABLE1.PRG",
-    "address": 0x00000000,
-    "page_address": 0x00000000
+    "file": None,
+    "filesize_formatted": None,
+    "filesize_hex": None,
+    "address": None,
+    "page_address": None
 }
 
 def draw_ui(screen):
@@ -37,7 +44,7 @@ def draw_ui(screen):
     curses.init_pair(COLOR_HEADER, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(COLOR_SUBHEADER, curses.COLOR_WHITE, curses.COLOR_RED)
     curses.init_pair(COLOR_ADDRESS, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(COLOR_ADDRESS_HIGHLIGHT, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(COLOR_ADDRESS_HIGHLIGHT, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(COLOR_TEXT, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(COLOR_TEXT_HIGHLIGHT, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(COLOR_MENU_NUMBERS, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -66,14 +73,27 @@ def draw_ui(screen):
         cursor_y = min(height-1, cursor_y)
 
         # Rendering some text
-        header_str = f"Selected : 00000000h - -= {PROGRAM_NAME} {VERSION} GPLv3+ Felipe Sanches {RELEASE_YEAR} =- - 0  -   {context['filesize']}"
-        subheader_str = f"{context['address']}/{context['filesize_hex']}  Hex        0%   {context['filename']}"
+        percentage = 100*context['address']/context['filesize']
+        percentage = f"{percentage:.1f}%"
+        header_str = f"Selected : 00000000h - -= {PROGRAM_NAME} {VERSION} GPLv3+ Felipe Sanches {RELEASE_YEAR} =- - 0  -   {context['filesize_formatted']}"
+        subheader_str = f"{context['address']}/{context['filesize_hex']}  Hex      {percentage}   {context['filename']}"
 
         def pad_str(s, width):
             return s + " " * (width - len(s) - 1)
 
         screen.addstr(0, 0, pad_str(header_str, width), curses.color_pair(COLOR_HEADER))
         screen.addstr(1, 0, pad_str(subheader_str, width), curses.color_pair(COLOR_SUBHEADER))
+
+        line_addr = context["page_address"]
+        for line_num in range(2,height-2):
+            if line_num == cursor_y:
+                addr_color = COLOR_ADDRESS_HIGHLIGHT
+            else:
+                addr_color = COLOR_ADDRESS
+
+            screen.addstr(line_num, 0, f'{line_addr:08X}', curses.color_pair(addr_color))
+            line_addr += 16
+
 
         # Render menu bar
         menu = { "1": "Info  ",
@@ -106,8 +126,34 @@ def draw_ui(screen):
         # Wait for next input
         key = screen.getch()
 
+
+def format_filesize(size):
+    if size > 1024*1024*1024:
+        return f"{size/(1024*1024*1024):.1f}G"
+    elif size > 1024*1024:
+        return f"{size/(1024*1024):.1f}M"
+    elif size > 1024:
+        return f"{size/1024:.1f}k"
+    else:
+        return size
+
+
+def load_file(filename):
+    context["filename"] = filename
+    context["file"] = open(filename, "rb")
+    context["filesize"] = os.path.getsize(context["filename"])
+    context["filesize_formatted"] = format_filesize(context["filesize"])
+    context["filesize_hex"] = f'{context["filesize"]:08X}'
+    context["address"] = 0x00000000
+    context["page_address"] = 0x00000000
+
+
 def main():
-    curses.wrapper(draw_ui)
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <filename>")
+    else:
+        load_file(sys.argv[1])
+        curses.wrapper(draw_ui)
 
 if __name__ == "__main__":
     main()
